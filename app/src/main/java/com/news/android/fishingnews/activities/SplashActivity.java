fd74942aba069f01f3653eef83eef8bab37727c4
朱瑞雪 <zhuruixue@hibaysoft.com>
2017-02-15 12:00:16 +0800

    no message

--- app/src/main/java/com/news/android/fishingnews/activities/SplashActivity.java
+++ app/src/main/java/com/news/android/fishingnews/activities/SplashActivity.java
@@ -2,6 +2,7 @@ package com.news.android.fishingnews.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.news.android.fishingnews.R;
 import com.news.android.fishingnews.utils.IntentUtil;
@@ -13,9 +14,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        findViewById(R.id.tv_text).setOnClickListener(v -> {
-            startActivity(IntentUtil.toMainActivity(this));
+        findViewById(R.id.tv_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(IntentUtil.toMainActivity(SplashActivity.this));
+            }
         });
-
     }
 }
